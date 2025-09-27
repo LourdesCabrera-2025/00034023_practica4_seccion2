78421312e76508db7381af4360efa4c7bbe8afcc
LourdesCabrera-2025 <00034023@uca.edu.sv>
2025-09-27 14:47:36 -0600

    :memo: :arrow_up: subiendo ejercicio 2 sobre tipo de variable

---
+++ Ejercicio 2/script_ejercicio2.js
@@ -0,0 +1,74 @@
+console.assert("Legolas" > "Gimli");
+console.clear();
+console.dir(window);
+console.dirxml(document.body);
+
+const marvel = ['Spider Man', 'Iron Man', 'Capitan America', 'Lobezno'];
+console.group('Superheroes de Marvel');
+marvel.forEach(superhero => console.log(superhero));
+console.groupEnd();
+
+var variable1;
+let variable2;
+const variable3 = 2;
+
+var $jquery, _private;
+
+var 50cent = 1, esto no vale = 1;
+
+var variable = 2, VARIABLE = 2;
+var camelCase = 2, snake_case = 2;
+var camelCase = 2;
+var snake_case = 3;
+
+var miVariable;
+let miOtraVariable;
+
+miVariable = 2;
+miOtraVariable = 2;
+
+var miVariable = 42;
+let miOtraVariable = miVariable;
+
+console.log(miVariable)
+var miVariable = 2;
+console.log(miVariable);
+
+console.log(miOtraVariable);
+let miOtraVariable = 2;
+console.log(miOtraVariable);
+
+const estoNoDaraProblemas = 42;
+
+
+const estoVaAFallar;
+const estoTambienFallara = 2;
+estoTambienFallara = 3;
+
+typeof variable;
+
+variable instanceof Clase;
+
+typeof undefined;
+typeof estaVariableQueNoHeInicializadoAun;
+
+typeof null;
+typeof {key : 2};
+typeof [15, 4];
+typeof new Date();
+
+typeof true;
+typeof false;
+typeof new Boolean(true);
+
+typeof "hola";
+
+typeof function(){};
+
+typeof 1;
+typeof 1.3;
+typeof NaN;
+typeof Infinity;
+
+typeof Symbol();
+typeof Symbol("Mi simbolo");
