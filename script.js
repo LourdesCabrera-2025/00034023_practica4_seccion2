8fe61d3a4002a27080b3edcf7ca3491b0a0d45ff
LourdesCabrera-2025 <00034023@uca.edu.sv>
2025-09-27 11:41:06 -0600

    :memo: subiendo archivos de practica de laboratorio 4

---
+++ script.js
@@ -0,0 +1,9 @@
+console.assert("Legolas" > "Gimli");
+console.clear();
+console.dir(window);
+console.dirxml(document.body);
+
+const marvel = ['Spider Man', 'Iron Man', 'Capitan America', 'Lobezno'];
+console.group('Superheroes de Marvel');
+marvel.forEach(superhero=> console.log(superhero));
+console.groupEnd();
