54d1f98470bc319d8bf4f2407d9cab49df84a369
LourdesCabrera-2025 <00034023@uca.edu.sv>
2025-09-27 23:09:04 -0600

    :memo: Subiendo practica de datos en consola

--- Ejercicio 2/script_ejercicio2.js
+++ Ejercicio 2/script_ejercicio2.js
@@ -8,20 +8,41 @@ console.group('Superheroes de Marvel');
 marvel.forEach(superhero => console.log(superhero));
 console.groupEnd();
 
-var miVariable;
-let miOtraVariable;
 
-miVariable = 2;
-miOtraVariable = 2;
+var Puntaje; // var permite redeclarar valores existentes
+let Score; //let no permite redeclarar valores ya existentes
 
-var miVariable = 42;
-let miOtraVariable2 = miVariable;
-/** se declara como miOtraVariable2 porque de lo contrario 
- * da Uncaught Error debido al reciclaje de variables en donde 
- * sabemos no se puede definir una variable ya existente
-*/
-console.log(miVariable);
-var miVariable = 2;
-console.log(miVariable);
+Puntaje = 4;
+Score = 10;
 
-console.log(miOtraVariable);
+var Puntaje = 165;
+let TotalScore = Puntaje;
+
+console.log(Puntaje);
+var Puntaje = 140;
+console.log(Puntaje);
+
+console.log(TotalScore);
+let FinalScore = 125;
+console.log(FinalScore);
+
+
+const AnyProblem = 45;
+const ThisProblem = 3;
+const AlmostFailed = 2;
+console.log(AnyProblem, ThisProblem, AlmostFailed);
+
+const variable = 15;
+console.log(typeof variable);
+const Products = ['Pizza' , 'macarons'];
+console.log(Products instanceof Array);
+console.log(Products instanceof Object);
+
+
+console.log (typeof undefined);
+
+console.log(typeof ValorKnown);
+console.log(typeof null);
+console.log(typeof {key: 3});
+console.log(typeof [15,4]);
+console.log(typeof new Date());
--- README.md
+++ README.md
@@ -8,8 +8,9 @@
 
 
 ## Captura de pantalla **Ejercicio_2**
-<p  style = "display:flex; justify-content: row; gap: 5px">
+<p  style = "display:grid; justify-content: center; gap: 5px">
 <img src = "image-2.png" alt = "Variables" witdh = 55>
+<br>
 <img src = "image-1.png" alt= "Resultado" witdh = "55"/>
 
 </p>
